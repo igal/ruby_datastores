4c89bf270c7950ce02f026df3fc0d334a7b6b410
Igal Koshevoy <igal@pragmaticraft.com>
2009-08-04 05:21:58 -0700

    Merged Jesse's CouchDB code, reformatted text of our discussion for easier reading.

--- couchdb_eg.rb
+++ couchdb_eg.rb
@@ -8,8 +8,7 @@ require 'couchrest'
 n = 1000
 db = CouchRest.database!("http://127.0.0.1:5984/couchrest-test")
 
-# You must define views in advance to look up documents by properties other
-# than '_id'.
+# Views define how to look up documents by properties other than '_id'.
 #
 # This view creates an index of key/value pairs where each document's number
 # attribute is a key and that document's data forms the corresponding value.
@@ -33,11 +32,11 @@ db.save_doc({
   }
 })
 
+# Instantiate some objects to store.
 items = []
 for i in 0...n
-  # FIXME I can't use integers?!
-  #
-  # Answer: You can use integers for any field except '_id'.
+  # Q: Why can't I use integers?
+  # A: You can use integers for any field except '_id'.
   items << { "_id" => i.to_s, "number" => i, :message => "omgkittens" }
 end
 
@@ -47,16 +46,18 @@ def elapsed(&block)
   return Time.now - timer
 end
 
+# Delete all non-design documents in a database.
+#
+# Q: Isn't there an easier way to delete everything?
+#
+# A: one good way is to drop the database. But then you would lose the
+# design document. Otherwise you have to have a copy of each document's
+# metadata in order to delete it because the document '_id' and '_rev'
+# attributes are required for the delete command. The presence of the '_rev'
+# attribute allows CouchDB to handle conflicts.
+#
+# But at least you don't have to fetch every document twice :)
 def delete_all(db)
-  # FIXME Isn't there an easier way to delete everything?
-  #
-  # Answer: one good way is to drop the database. But then you would lose the
-  # design document. Otherwise you have to have a copy of each document's
-  # metadata in order to delete it because the document '_id' and '_rev'
-  # attributes are required for the delete command. The presence of the '_rev'
-  # attribute allows CouchDB to handle conflicts.
-  #
-  # But at least you don't have to fetch every document twice :)
   db.documents['rows'].each do |row|
     unless row['id'] =~ /^_design/  # Don't delete the design document.
       db.delete_doc({ '_id' => row['id'], '_rev' => row['value']['rev'] }, true)
@@ -69,7 +70,7 @@ def delete_all(db)
   result_set['total_rows'] == 1 or raise "Expected all documents to be deleted, got #{result_set.inspect}"
 end
 
-# individual inserts
+# Perform individual inserts.
 delete_all(db)
 s = elapsed do
   for i in 0...n
@@ -78,19 +79,21 @@ s = elapsed do
 end
 puts "* %i inserts per second individually, for %i items over %0.2f seconds" % [n/s, n, s]
 
-# FIXME How do I perform a bulk insert? The #bulk_save method throws a cryptic exception.
+# Perform bulk inserts.
 #
-# Answer: an id was passed to `save_doc` where there should have been a document.
+# Q: How do I perform a bulk insert? The #bulk_save method throws a cryptic exception.
+#
+# A: an id was passed to `save_doc` where there should have been a document.
 delete_all(db)
 s = elapsed do
   items.each do |item|
     db.save_doc(item, true)
   end
-  db.bulk_save # FIXME wtf? throws an exception about Fixnums
+  db.bulk_save
 end
 puts "* %i inserts per second as group, for %i items over %0.2f seconds" % [n/s, n, s]
 
-# lookups by '_id'
+# Perform lookups by '_id'.
 s = elapsed do
   for i in 0...n
     item = db.get(i.to_s)
@@ -99,9 +102,11 @@ s = elapsed do
 end
 puts "* %i retrieves per second by '_id', for %i items over %0.2f seconds" % [n/s, n, s]
 
-# FIXME How do I retrieve records by the "number" column? Do I have to create a per-lookup view?
+# Retrieve documents by 'number'.
 #
-# Answer: you have to create views in advance for any column / key that you
+# Q: How do I retrieve records by the "number" column? Do I have to create a per-lookup view?
+#
+# A: you have to create views in advance for any column / key that you
 # want to use to perform lookups.  But the value to match with that key and
 # other options are provided at query time.
 s = elapsed do
@@ -117,9 +122,10 @@ s = elapsed do
 end
 puts "* %i retrieves per second by 'number', for %i items over %0.2f seconds" % [n/s, n, s]
 
-# FIXME How do I retrieve multiple records at once efficiently?
+# Retrieve multiple documents.
 #
-# Answer: compose a query with a key range instead of a single key. Key ranges
+# Q: How do I retrieve multiple records at once efficiently?
+# A: compose a query with a key range instead of a single key. Key ranges
 # on array keys can be useful for more complicated queries.
 #
 # If you are using CouchDB 0.9 or later you can also use the all_docs view with
@@ -141,4 +147,12 @@ actual = result_set['rows'].reduce(0) { |sum, row|
 }
 actual == expected or raise "Mismatch! Expected #{expected}, got: #{result_set.inspect}"
 
-puts "* %i retrieves per second by 'name' as group, for %i items over %0.2f seconds" % [n/s, n, s]
+puts "* %i retrieves per second as group, for %i items over %0.2f seconds" % [n/s, n, s]
+
+=begin
+* 176 inserts per second individually, for 1000 items over 5.65 seconds
+* 1620 inserts per second as group, for 1000 items over 0.62 seconds
+* 404 retrieves per second by '_id', for 1000 items over 2.47 seconds
+* 237 retrieves per second by 'number', for 1000 items over 4.21 seconds
+* 9394 retrieves per second as group, for 1000 items over 0.11 seconds
+=end
