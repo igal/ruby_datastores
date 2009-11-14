2f9d9149d74520a190c528fc80aa722002fc33d9
Igal Koshevoy <igal@pragmaticraft.com>
2009-11-13 19:38:51 -0800

    Updated mongo_eg to 0.16 driver's API.

--- mongo_eg.rb
+++ mongo_eg.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'rubygems'
-require 'mongo'
+require 'mongo' # Using mongo 0.16 driver from GemCutter
 
 #---[ Data structures ]-------------------------------------------------
 
@@ -20,25 +20,25 @@ tasks = [
 #---[ Mongo ]-----------------------------------------------------------
 
 # Connect to database
-db = XGen::Mongo::Driver::Mongo.new("localhost", 27017).db("mydb")
+db = Mongo::Connection.new("localhost", 27017).db("mydb")
 
 # Populate categories
 category_collection = db.collection("category")
-category_collection.clear
+category_collection.remove
 categories.each do |category|
   category_collection << category
 end
 
 # Populate tasks
 task_collection = db.collection("task")
-task_collection.clear
+task_collection.remove
 tasks.each do |task|
   task_collection << task
 end
 task_collection.create_index("category_id")
 
 # Find task by id
-task = task_collection.find(:_id => 1).first
+task = task_collection.find_one(:_id => 1)
 p task
 
 # Find tasks by category
@@ -67,7 +67,7 @@ for i in 0...n
   items << { :id => i, :message => "omgkittens" }
 end
 
-benchmark_collection.clear
+benchmark_collection.remove
 s = elapsed do
   for i in 0...n
     benchmark_collection << items[i]
@@ -75,56 +75,70 @@ s = elapsed do
 end
 puts "* %i inserts per second individually, for %i items over %0.2f seconds" % [n/s, n, s]
 
-#benchmark_collection.clear
-#s = elapsed do
-#  benchmark_collection.insert(items)
-#end
-#puts "* %i inserts per second as group, for %i items over %0.2f seconds" % [n/s, n, s]
+benchmark_collection.remove
+s = elapsed do
+  benchmark_collection.insert(items)
+end
+puts "* %i inserts per second as group, for %i items over %0.2f seconds" % [n/s, n, s]
 
 system "sync"
 
 s = elapsed do
   for i in 0...n
-    item = benchmark_collection.find("id" => i).first
+    item = benchmark_collection.find_one("id" => i)
     item["id"] == i or raise "Mismatch! Expected #{i}, got: #{item.inspect}"
   end
 end
 puts "* %i retrieves per second individually, for %i items over %0.2f seconds" % [n/s, n, s]
 
-# seen = {}
-# s = elapsed do
-#   benchmark_collection.find.each do |item|
-#     i = item["id"]
-#     if seen[i]
-#       raise "Already saw #{i}"
-#     else
-#       seen[i] = true
-#     end
-#   end
-# end
-# puts "* %i retrieves per second as group, for %i items over %0.2f seconds" % [n/s, n, s]
+seen = {}
+s = elapsed do
+  benchmark_collection.find.each do |item|
+    i = item["id"]
+    if seen[i]
+      raise "Already saw #{i}"
+    else
+      seen[i] = true
+    end
+  end
+end
+puts "* %i retrieves per second as group, for %i items over %0.2f seconds" % [n/s, n, s]
 
 
 #---[ Teardown ]--------------------------------------------------------
 
-category_collection.clear
-task_collection.clear
-benchmark_collection.clear
+category_collection.remove
+task_collection.remove
+benchmark_collection.remove
 
 =begin
-MRI 1.8.7
+MRI 1.8.7 with mongo 0.10 driver
 * 3316 inserts per second individually, for 10000 items over 3.01 seconds
 * 917 inserts per second as group, for 10000 items over 11.36 seconds
 * 1375 retrieves per second individually, for 10000 items over 7.27 seconds
 * 7725 retrieves per second as group, for 10000 items over 1.29 seconds
 
-JRuby 1.3.1
+JRuby 1.3.1 with mongo 0.10 driver
 * 3892 inserts per second individually, for 10000 items over 2.57 seconds
 * 7158 inserts per second as group, for 10000 items over 1.40 seconds
 * 1845 retrieves per second individually, for 10000 items over 5.42 seconds
 * 6963 retrieves per second as group, for 10000 items over 1.44 seconds
 
-JRuby 1.4.0
+JRuby 1.4.0 with mongo 0.10 driver
 * 4366 inserts per second individually, for 10000 items over 2.29 seconds
 * 1680 retrieves per second individually, for 10000 items over 5.95 seconds
+
+...
+
+MRI 1.8.7 with mongo 0.16 driver
+* 2376 inserts per second individually, for 10000 items over 4.21 seconds
+* 1018 inserts per second as group, for 10000 items over 10.12 seconds
+* 1049 retrieves per second individually, for 10000 items over 9.53 seconds
+* 3458 retrieves per second as group, for 10000 items over 2.89 seconds
+
+JRuby 1.4.0 with mongo 0.16 driver
+* 2374 inserts per second individually, for 10000 items over 4.21 seconds
+* 8928 inserts per second as group, for 10000 items over 1.12 seconds
+* 1819 retrieves per second individually, for 10000 items over 5.49 seconds
+* 6116 retrieves per second as group, for 10000 items over 1.63 seconds
 =end
